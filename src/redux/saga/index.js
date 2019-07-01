914c446926dc790e7f47ae70f9b7915fdbc9b2c5
Richard Ng <richardcrng@gmail.com>
2019-07-02 00:07:54 +0100

    create saga to handle special keys

--- src/redux/saga/index.js
+++ src/redux/saga/index.js
@@ -4,6 +4,9 @@ import server from '../../server';
 import filterT9Predictions from '../../server/predictions/filter';
 
 export function* processKeyClick({ payload: key }) {
+  if ([0, '*', '#'].includes(key)) {
+    return yield call(handleSpecialKey, key)
+  }
   yield put(actions.numericString.create.concat(`${key}`))
   const prevUnfilteredArr = yield select(state => state.unfilteredWords)
   const unfilteredArr = yield call(server.predictions.get, [key, prevUnfilteredArr])
@@ -16,6 +19,35 @@ export function* processKeyClick({ payload: key }) {
 
 processKeyClick.TRIGGER = 'TRIGGER_SAGA: processKeyClick'
 processKeyClick.trigger = payload => ({ type: processKeyClick.TRIGGER, payload })
+
+function* handleSpecialKey(key) {
+  switch (key) {
+    case 0:
+      yield put(actions.shouldFilter.create.toggle())
+      break
+
+    case '*':
+      yield all([
+        actions.numericString.create.reset(),
+        actions.unfilteredWords.create.reset(),
+        actions.filteredWords.create.reset()
+      ].map(action => put(action)));
+      break
+
+    case '#':
+      yield put(actions.numericString.create.slice([0, -1]))
+      const newString = yield select(state => state.numericString)
+      const unfilteredArr = yield call(server.predictions.get, newString)
+      const filteredArr = yield call(filterT9Predictions, unfilteredArr)
+      yield all([
+        put(actions.unfilteredWords.create.update(unfilteredArr)),
+        put(actions.filteredWords.create.update(filteredArr))
+      ])
+      break
+      
+    default: break;
+  }
+}
 
 function* rootSaga() {
   yield takeEvery(processKeyClick.TRIGGER, processKeyClick)
