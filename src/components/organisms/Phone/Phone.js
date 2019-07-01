f83a4bdc379fca12066f076ea2dd24053b6fd495
Richard Ng <richardcrng@gmail.com>
2019-07-01 22:33:45 +0100

    create phone context

--- src/components/organisms/Phone/Phone.js
+++ src/components/organisms/Phone/Phone.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Display from '../../atoms/Display';
 import Keypad from '../../molecules/Keypad/Keypad';
 
-function Phone({ numericString, wordsArr }) {
+const PhoneContext = React.createContext()
+export const usePhoneKeyClickHandler = () => React.useContext(PhoneContext).onKeyClick
+
+function Phone({ onKeyClick, numericString, wordsArr }) {
   return (
-    <>
+    <PhoneContext.Provider value={{ onKeyClick }}>
       <Display {...{ numericString, wordsArr }} />
       <Keypad />
-    </>
+    </PhoneContext.Provider>
   )
 }
 
