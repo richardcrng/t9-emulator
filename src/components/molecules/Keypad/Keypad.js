739b4511eca8c28a0e99f7ff553dc4a731dac769
Richard Ng <richardcrng@gmail.com>
2019-07-01 21:47:29 +0100

    create KeypadContext provider

--- src/components/molecules/Keypad/Keypad.js
+++ src/components/molecules/Keypad/Keypad.js
@@ -3,25 +3,30 @@ import classes from './Keypad.module.css'
 import Key from '../../atoms/Key';
 import KeyRow from '../KeyRow';
 
+export const KeypadContext = React.createContext()
+KeypadContext.displayName = 'KeypadContext'
+
 function Keypad() {
   return (
-    <div className={classes.Keypad}>
-      <KeyRow>
-        <Key n={1} />
-        <Key n={2} />
-        <Key n={3} />
-      </KeyRow>
-      <KeyRow>
-        <Key n={4} />
-        <Key n={5} />
-        <Key n={6} />
-      </KeyRow>
-      <KeyRow>
-        <Key n={7} />
-        <Key n={8} />
-        <Key n={9} />
-      </KeyRow>
-    </div>
+    <KeypadContext.Provider value={{ nRows: 3 }}>
+      <div className={classes.Keypad}>
+        <KeyRow>
+          <Key n={1} />
+          <Key n={2} />
+          <Key n={3} />
+        </KeyRow>
+        <KeyRow>
+          <Key n={4} />
+          <Key n={5} />
+          <Key n={6} />
+        </KeyRow>
+        <KeyRow>
+          <Key n={7} />
+          <Key n={8} />
+          <Key n={9} />
+        </KeyRow>
+      </div>
+    </KeypadContext.Provider>
   )
 }
 
