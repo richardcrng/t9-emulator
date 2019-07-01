121c5f7d86e5f5066910b7cf8d304040ce63203c
Richard Ng <richardcrng@gmail.com>
2019-07-01 22:47:52 +0100

    setup saga in store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,18 @@
-import { configureStore } from 'redux-starter-kit';
+import createSagaMiddleware from 'redux-saga';
+import { configureStore, getDefaultMiddleware } from 'redux-starter-kit';
 import { reducer } from '../leaves';
+import rootSaga from '../saga';
 
-const store = configureStore({ reducer })
+const createStore = () => {
+  const sagaMiddleware = createSagaMiddleware()
+  const middleware = [...getDefaultMiddleware(), sagaMiddleware]
+  return {
+    ...configureStore({ reducer, middleware }),
+    runSaga: sagaMiddleware.run
+  }
+}
+
+const store = createStore()
+store.runSaga(rootSaga)
 
 export default store
