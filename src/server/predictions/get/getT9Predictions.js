f557185ee4b247deeb0cd156f5f9ba448d77d466
Richard Ng <richardcrng@gmail.com>
2019-07-01 15:21:18 +0100

    comments on growPossibilities

--- src/server/predictions/get/getT9Predictions.js
+++ src/server/predictions/get/getT9Predictions.js
@@ -21,6 +21,20 @@ const getT9Predictions = numericString => {
   )
 }
 
+const growPossibilities = (accumulatedPossibilities, lettersArr) => {
+  // if no accumulated possibilities, then there are no possibilities to grow
+  //    - so return the lettersArr as the only possibilities for now
+  if (accumulatedPossibilities.length === 0) return lettersArr
+
+  // otherwise, map each possibility onto an array where we augment by
+  //    the lettersArr; should be a flatMap so that we end up with a
+  //    one-dimensional array of all possibilities
+  return _.flatMap(
+    accumulatedPossibilities,
+    possibility => augmentPrediction(lettersArr, possibility)
+  )
+}
+
 /**
  * Converts a numeric string of n letters into an array of n elements, 
  * where each element is an array of letters
