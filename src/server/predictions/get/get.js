c693d52b26165ad23d936be23c8c625e875c3a43
Richard Ng <richardcrng@gmail.com>
2019-07-01 14:16:47 +0100

    add jsdoc to get

--- src/server/predictions/get/get.js
+++ src/server/predictions/get/get.js
@@ -1,4 +1,10 @@
-function getT9Predictions(arr = []) {
+/**
+ * Converts a given numeric string into an array of T9 predictive text matches
+ * 
+ * @param {String} numericString - A string of numeric input to convert into T9, e.g. '23'
+ * @return {String[]} An array of predictive text matches
+ */
+function getT9Predictions(numericString) {
   return null
 }
 
