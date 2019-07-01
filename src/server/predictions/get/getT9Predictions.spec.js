7a71e01339a1167c83804092566e8862dd5d7636
Richard Ng <richardcrng@gmail.com>
2019-07-01 23:24:56 +0100

    write tests for array input for getT9Predictions

--- src/server/predictions/get/getT9Predictions.spec.js
+++ src/server/predictions/get/getT9Predictions.spec.js
@@ -1,6 +1,6 @@
 import getT9Predictions from ".";
 
-describe("getT9Predictions: provides array of predictions given a numeric input", () => {
+describe("getT9Predictions for numeric string input", () => {
   describe("GIVEN the input '23'", () => {
     const input = '23'
 
@@ -17,6 +17,58 @@ describe("getT9Predictions: provides array of predictions given a numeric input"
 
   describe("GIVEN the input '228'", () => {
     const input = '228'
+
+    describe("WHEN we pass the input to getT9Predictions and don't filter", () => {
+      const result = getT9Predictions(input)
+
+      test("THEN it returns an unfiltered array", () => {
+        expect(result).toEqual([
+          'aat', 'aau', 'aav',
+          'abt', 'abu', 'abv',
+          'act', 'acu', 'acv',
+          'bat', 'bau', 'bav',
+          'bbt', 'bbu', 'bbv',
+          'bct', 'bcu', 'bcv',
+          'cat', 'cau', 'cav',
+          'cbt', 'cbu', 'cbv',
+          'cct', 'ccu', 'ccv'
+        ])
+      })
+    })
+
+    describe("WHEN we pass the input to getT9Predictions and filter", () => {
+      const result = getT9Predictions(input, true)
+
+      test("THEN it returns a filtered array", () => {
+        expect(result).toEqual(['abu', 'act', 'bat', 'cat'])
+      })
+    })
+  })
+})
+
+describe("getT9Predictions for array input", () => {
+  describe("GIVEN the input [['a', 'b', 'c'], '3']", () => {
+    const input = [
+      ['a', 'b', 'c'],
+      '3'
+    ]
+
+    describe("WHEN we pass the input to getT9Predictions", () => {
+      const result = getT9Predictions(input)
+
+      test("THEN it returns the array ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']", () => {
+        expect(result).toEqual([
+          'ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'
+        ])
+      })
+    })
+  })
+
+  describe("GIVEN the input [['aa', 'ab', 'ac', 'ba', 'bb', 'bc', 'ca', 'cb', 'cc'],'8']", () => {
+    const input = [
+      ['aa', 'ab', 'ac', 'ba', 'bb', 'bc', 'ca', 'cb', 'cc'],
+      '8'
+    ]
 
     describe("WHEN we pass the input to getT9Predictions and don't filter", () => {
       const result = getT9Predictions(input)
