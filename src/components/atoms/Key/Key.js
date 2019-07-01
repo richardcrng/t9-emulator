4083a864ca869f4febc6383ea04e8c0ed4c5bdab
Richard Ng <richardcrng@gmail.com>
2019-07-01 21:21:45 +0100

    add dummy key content

--- src/components/atoms/Key/Key.js
+++ src/components/atoms/Key/Key.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-function Key() {
-  return null
+function Key({ n, letters }) {
+  return (
+    <div>
+      <span>
+        {n}
+      </span>
+      <span>
+        {letters}
+      </span>
+    </div>
+  )
 }
 
 export default Key;
