fb77eb971d462d58204c1dedad1a1d906a6204a7
Richard Ng <richardcrng@gmail.com>
2019-07-01 14:27:17 +0100

    write first failing test

--- src/server/predictions/get/getT9Predictions.spec.js
+++ src/server/predictions/get/getT9Predictions.spec.js
@@ -1,5 +1,17 @@
+import getT9Predictions from ".";
+
 describe("getT9Predictions", () => {
-  it("tests something", () => {
-    expect(5).toBe(5)
+  describe("GIVEN the input '23'", () => {
+    const input = '23'
+
+    describe("WHEN we pass the input to getT9Predictions", () => {
+      const result = getT9Predictions(input)
+
+      test("THEN it returns the array ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']", () => {
+        expect(result).toEqual([
+          'ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'
+        ])
+      })
+    })
   })
 })
