915727831cd6637690b2889ef8f9e55cad102c2f
Richard Ng <richardcrng@gmail.com>
2019-07-01 14:50:55 +0100

    test passing for augmentPredictions

--- src/server/predictions/get/getT9Predictions.spec.js
+++ src/server/predictions/get/getT9Predictions.spec.js
@@ -1,7 +1,8 @@
 import getT9Predictions from ".";
+import { augmentPrediction } from "./getT9Predictions";
 
 describe("getT9Predictions", () => {
-  describe("GIVEN the input '23'", () => {
+  describe.skip("GIVEN the input '23'", () => {
     const input = '23'
 
     describe("WHEN we pass the input to getT9Predictions", () => {
@@ -11,6 +12,26 @@ describe("getT9Predictions", () => {
         expect(result).toEqual([
           'ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'
         ])
+      })
+    })
+  })
+})
+
+describe("augmentPredictions", () => {
+  describe("GIVEN an array of letters to follow, ['d', 'e', 'f']", () => {
+    const lettersArr = ['d', 'e', 'f']
+
+    describe("AND an existing prediction, 'a'", () => {
+      const prediction = 'a'
+
+      describe("WHEN augmentPredictions is called with these two arguments", () => {
+        const result = augmentPrediction(lettersArr, prediction)
+
+        test("THEN the result is ['ad', 'ae', 'af']", () => {
+          expect(result).toEqual([
+            'ad', 'ae', 'af'
+          ])
+        })
       })
     })
   })
