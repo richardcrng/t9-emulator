d3e1329fdf8d959640c65a442923ad018c33ce4f
Richard Ng <richardcrng@gmail.com>
2019-07-01 21:25:12 +0100

    fill out Keypad with some static Keys

--- src/components/molecules/Keypad/Keypad.js
+++ src/components/molecules/Keypad/Keypad.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Key from '../../atoms/Key';
 
 function Keypad() {
-  return null
+  return (
+    <div>
+      <div>
+        <Key n={1} />
+        <Key n={2} />
+        <Key n={3} />
+      </div>
+      <div>
+        <Key n={4} />
+        <Key n={5} />
+        <Key n={6} />
+      </div>
+      <div>
+        <Key n={7} />
+        <Key n={8} />
+        <Key n={9} />
+      </div>
+    </div>
+  )
 }
 
 export default Keypad;
