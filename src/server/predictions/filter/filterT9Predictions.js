ffeeba9c08221a23a374c695ef17816fd75d7921
Richard Ng <richardcrng@gmail.com>
2019-07-04 12:42:57 +0100

    default argument of empty array in filterT9Predictions

--- src/server/predictions/filter/filterT9Predictions.js
+++ src/server/predictions/filter/filterT9Predictions.js
@@ -2,6 +2,6 @@ import top10000words from "./top10000words";
 
 const wordsArr = top10000words.split('\n')
 
-const filterT9Predictions = arr => arr.filter(str => wordsArr.includes(str))
+const filterT9Predictions = (arr = []) => arr.filter(str => wordsArr.includes(str))
 
 export default filterT9Predictions
